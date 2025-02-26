2ab8d57b30994d7fdc558a744ffec988cd2752ee
annandajacobs <annandajacobs@gmail.com>
2025-02-26 17:39:18 -0300

    update06

---
+++ br/com/exemplo/proj/principal/PrincipalComBusca.java
@@ -0,0 +1,38 @@
+package br.com.exemplo.proj.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+
+import br.com.exemplo.proj.modelos.Titulo;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=9f32a5d0";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+         .uri(URI.create(endereco))
+         .build();
+
+         HttpResponse<String> response = client
+        .send(request, BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
+    }
+}
